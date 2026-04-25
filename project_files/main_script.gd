df8a1a4a62143f08108c24c931c2fe7a6cc7a44e
Jan Piętka <pientaszek007@gmail.com>
2026-04-25 18:14:14 +0200

    zmiana skryptu teleportu

--- project_files/main_script.gd
+++ project_files/main_script.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 var webxr_interface
 @onready var action_label: Label3D = $XROrigin3D/LeftController/ActionLabel
+@onready var left_controller: XRController3D = $XROrigin3D/LeftController
 
 func _ready() -> void:
 	$CanvasLayer.visible = false
@@ -9,8 +10,6 @@ func _ready() -> void:
 
 	webxr_interface = XRServer.find_interface("WebXR")
 	if webxr_interface:
-	# WebXR uses a lot of asynchronous callbacks, so we connect to various
-	# signals in order to receive them.
 		webxr_interface.session_supported.connect(self._webxr_session_supported)
 		webxr_interface.session_started.connect(self._webxr_session_started)
 		webxr_interface.session_ended.connect(self._webxr_session_ended)
@@ -24,13 +23,11 @@ func _ready() -> void:
 		webxr_interface.squeezestart.connect(self._webxr_on_squeeze_start)
 		webxr_interface.squeezeend.connect(self._webxr_on_squeeze_end)
 
-	# This returns immediately - our _webxr_session_supported() method
-	# (which we connected to the "session_supported" signal above) will
-	# be called sometime later to let us know if it's supported or not.
 		webxr_interface.is_session_supported("immersive-vr")
 
-	$XROrigin3D/LeftController.button_pressed.connect(self._on_left_controller_button_pressed)
-	$XROrigin3D/LeftController.button_released.connect(self._on_left_controller_button_released)
+	# Łączenie sygnałów przycisków kontrolera [cite: 4]
+	left_controller.button_pressed.connect(self._on_left_controller_button_pressed)
+	left_controller.button_released.connect(self._on_left_controller_button_released)
 
 func _webxr_session_supported(session_mode: String, supported: bool) -> void:
 	if session_mode == 'immersive-vr':
@@ -40,42 +37,22 @@ func _webxr_session_supported(session_mode: String, supported: bool) -> void:
 			OS.alert("Your browser doesn't support VR")
 
 func _on_button_pressed() -> void:
-  # We want an immersive VR session, as opposed to AR ('immersive-ar') or a
-  # simple 3DoF viewer ('viewer').
 	webxr_interface.session_mode = 'immersive-vr'
-  # 'bounded-floor' is room scale, 'local-floor' is a standing or sitting
-  # experience (it puts you 1.6m above the ground if you have 3DoF headset),
-  # whereas as 'local' puts you down at the ARVROrigin.
-  # This list means it'll first try to request 'bounded-floor', then
-  # fallback on 'local-floor' and ultimately 'local', if nothing else is
-  # supported.
 	webxr_interface.requested_reference_space_types = 'bounded-floor, local-floor, local'
-  # In order to use 'local-floor' or 'bounded-floor' we must also
-  # mark the features as required or optional.
 	webxr_interface.required_features = 'local-floor'
 	webxr_interface.optional_features = 'bounded-floor'
 
-  # This will return false if we're unable to even request the session,
-  # however, it can still fail asynchronously later in the process, so we
-  # only know if it's really succeeded or failed when our
-  # _webxr_session_started() or _webxr_session_failed() methods are called.
 	if not webxr_interface.initialize():
 		OS.alert("Failed to initialize WebXR")
 		return
 
 func _webxr_session_started() -> void:
 	$CanvasLayer.visible = false
-  # This tells Godot to start rendering to the headset.
 	get_viewport().use_xr = true
-  # This will be the reference space type you ultimately got, out of the
-  # types that you requested above. This is useful if you want the game to
-  # work a little differently in 'bounded-floor' versus 'local-floor'.
 	print ("Reference space type: " + webxr_interface.reference_space_type)
 
 func _webxr_session_ended() -> void:
 	$CanvasLayer.visible = true
-  # If the user exits immersive mode, then we tell Godot to render to the web
-  # page again.
 	get_viewport().use_xr = false
 
 func _webxr_session_failed(message: String) -> void:
@@ -83,39 +60,53 @@ func _webxr_session_failed(message: String) -> void:
 
 func _on_left_controller_button_pressed(button: String) -> void:
 	print ("Button pressed: " + button)
-	# Wyświetlenie nazwy wciśniętego przycisku w przestrzeni 3D
+	
+	# Wyświetlanie nazwy na etykiecie 3D
 	if action_label:
 		action_label.text = "Wcisnieto: " + button
+	
+	# Start celowania teleportem po wciśnięciu triggera
+	if button == "trigger_click":
+		if left_controller.has_method("start_aiming"):
+			left_controller.start_aiming()
 
 func _on_left_controller_button_released(button: String) -> void:
 	print ("Button release: " + button)
-	# Wyświetlenie nazwy puszczonego przycisku w przestrzeni 3D
+	
+	# Wyświetlanie nazwy na etykiecie 3D
 	if action_label:
 		action_label.text = "Puszczono: " + button
+		
+	# Wykonanie teleportacji po puszczeniu triggera
+	if button == "trigger_click":
+		if left_controller.has_method("teleport_now"):
+			left_controller.teleport_now()
+		
+		# Zatrzymanie celowania (ukrycie linii/markera)
+		if left_controller.has_method("stop_aiming"):
+			left_controller.stop_aiming()
 
 func _process(_delta: float) -> void:
-	var thumbstick_vector: Vector2 = $XROrigin3D/LeftController.get_vector2("thumbstick")
+	var thumbstick_vector: Vector2 = left_controller.get_vector2("thumbstick")
 	if thumbstick_vector != Vector2.ZERO:
-		print ("Left thumbstick position: " + str(thumbstick_vector))
+		# Logowanie pozycji drążka [cite: 14]
+		pass 
 
 func _webxr_on_select(input_source_id: int) -> void:
-	print("Select: " + str(input_source_id))
-
 	var tracker: XRPositionalTracker = webxr_interface.get_input_source_tracker(input_source_id)
 	var xform = tracker.get_pose('default').transform
-	print (xform.origin)
 
 func _webxr_on_select_start(input_source_id: int) -> void:
-	print("Select Start: " + str(input_source_id))
+	pass
 
 func _webxr_on_select_end(input_source_id: int) -> void:
-	print("Select End: " + str(input_source_id))
+	pass
 
 func _webxr_on_squeeze(input_source_id: int) -> void:
-	print("Squeeze: " + str(input_source_id))
+	pass
 
 func _webxr_on_squeeze_start(input_source_id: int) -> void:
-	print("Squeeze Start: " + str(input_source_id))
+	pass
 
 func _webxr_on_squeeze_end(input_source_id: int) -> void:
-	print("Squeeze End: " + str(input_source_id))
+	pass
--- project_files/teleport.gd
+++ project_files/teleport.gd
@@ -5,30 +5,48 @@ extends XRController3D
 var xr_origin: XROrigin3D
 var xr_camera: XRCamera3D
 
+var is_aiming: bool = false # Zmienna sprawdzająca, czy gracz celuje
+
 func _ready() -> void:
 	xr_origin = get_parent() as XROrigin3D
 	xr_camera = xr_origin.get_node("XRCamera3D") as XRCamera3D
+	
+	# Domyślnie wyłączamy promień i znacznik
+	ray.enabled = false
+	marker.visible = false
+
+# Funkcja włączająca celowanie
+func start_aiming() -> void:
+	is_aiming = true
+	ray.enabled = true
+
+# Funkcja wyłączająca celowanie
+func stop_aiming() -> void:
+	is_aiming = false
+	ray.enabled = false
 	marker.visible = false
 
 func _process(_delta: float) -> void:
-	if ray.is_colliding():
+	# Aktualizujemy pozycję markera TYLKO gdy celujemy
+	if is_aiming and ray.is_colliding():
 		marker.global_transform.origin = ray.get_collision_point()
 		marker.visible = true
 	else:
 		marker.visible = false
 
 func teleport_now() -> void:
-	if not ray.is_colliding():
+	# Przerywamy, jeśli gracz nie celuje lub promień w nic nie uderza
+	if not is_aiming or not ray.is_colliding():
 		return
+		
 	var target: Vector3 = ray.get_collision_point()
 
 	var origin_tf := xr_origin.global_transform
 	var cam_tf := xr_camera.global_transform
 	var cam_offset := cam_tf.origin - origin_tf.origin
 	
-	# Height Sabilisation
-	#cam_offset.y = 0.0
-	#origin_tf.origin = Vector3(target.x - cam_offset.x, target.y, target.z - cam_offset.z)
-	
 	origin_tf.origin = target - cam_offset
 	xr_origin.global_transform = origin_tf
+	
+	# Po udanej teleportacji wyłączamy celowanie
+	stop_aiming()
